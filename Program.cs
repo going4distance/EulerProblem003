533193e72573f11850338c01d6100b09838c2392
Going4Distance <36748771+going4distance@users.noreply.github.com>
2018-11-25 16:26:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Csharp_Euler003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*  Euler Challenge #3
+                What is the largest prime factor of the number 600851475143 ?   
+                https://projecteuler.net/problem=3  */
+
+            int bigPrime = 1; long numBig = 600851475143;
+            int currFactor = 2;
+            while (numBig > 1)
+            {
+                if(numBig % currFactor == 0)
+                {
+                    numBig = numBig / currFactor;
+                    if(currFactor > bigPrime){
+                        bigPrime = currFactor;
+                    }
+                }
+                else
+                {
+                    currFactor++;
+                }
+            }
+            Console.WriteLine("Euler Challenge #3\n\nThe largest prime factor of the number 600851475143 = {0}.", bigPrime);
+            Console.ReadLine();
+        }
+    }
+}
